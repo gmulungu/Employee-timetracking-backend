edf9779181f11818ef88bf80de850e33bdc4b034
Geurschomm <geurschom.mulungu@redpandasoftware.co.za>
2025-02-03 09:35:29 +0200

    the change of password works

--- Models/LoginDto.cs
+++ Models/LoginDto.cs
@@ -1,5 +1,6 @@
 public class LoginDto
 {
-    public string Username { get; set; }
+    // public string Username { get; set; }
     public string Password { get; set; }
+    public int employeeNo { get; set; }
 }
